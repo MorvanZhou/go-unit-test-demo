fedaeba25b85f29a195a083e2e1260b9a1f218c1
morvanzhou <morvanzhou@tencent.com>
2019-11-14 20:16:05 +0800

    update

--- 5mock/foo.go
+++ 5mock/foo.go
@@ -1,15 +1,16 @@
-//go:generate mockgen -destination=mock_foo.go -package=_mock github.com/morvanzhou/unittest-demo/5mock MyInterface
+//go:generate mockgen -destination=mock_foo.go -package=_mock github.com/morvanzhou/unittest-demo/5mock YourInterface
 
 package _mock
 
-type MyInterface interface {
-	MyInterFunc(ii int) int
+type YourInterface interface {
+	YourInterFunc(ii int) int
 }
 
 type OtherInterface interface {
 	OtherInterfaceFunc(ii int) int
 }
 
-func Myfunc(instance MyInterface, mi int) int {
-	return instance.MyInterFunc(mi)
+func Myfunc(instance YourInterface, mi int) int {
+	mi = mi - 1
+	return instance.YourInterFunc(mi)
 }
--- 5mock/foo_test.go
+++ 5mock/foo_test.go
@@ -8,47 +8,53 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMyfunc(t *testing.T) {
+func TestMyFunc(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	// get mocked interface
-	m := _mock.NewMockMyInterface(ctrl)
+	m := _mock.NewMockYourInterface(ctrl)
 
 	// set test case
 	m.EXPECT().
-		MyInterFunc(gomock.Eq(1)).		// when use MyInterFunc(1)
-		Return(101).					// should return 101
+		YourInterFunc(gomock.Eq(1)). // when use MyInterFunc(1)
+		Return(101).                 // should return 101
 		AnyTimes()
 
 	m.EXPECT().
-		MyInterFunc(gomock.Not(1)).		// when use MyInterFunc(other than 1)
-		Return(100).					// should return 100
+		YourInterFunc(gomock.Not(1)). // when use MyInterFunc(other than 1)
+		Return(100).                  // should return 100
 		AnyTimes()
 
-	assert.Equal(t, 101, _mock.Myfunc(m, 1))
+	assert.Equal(t, 100, _mock.Myfunc(m, 1))
 	assert.Equal(t, 100, _mock.Myfunc(m, 98))
+	assert.Equal(t, 101, _mock.Myfunc(m, 2))
 }
 
-func TestMyfuncOrder(t *testing.T) {
+func TestMyFuncOrder(t *testing.T) {
 	tt := []struct {
 		name   string
 		input  int
 		expect int
 	}{
-		{"test1", 1, 1},
-		{"test2", 2, -1},
-		{"test3", 33, -1},
+		{"test1", 2, 1},
+		{"test3", 3, 2},
+		{"test2", 1, -1},
+
+		//{"test4", 33, -1},
+		//{"test5", 2, 1},
+
 	}
 
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	m := _mock.NewMockMyInterface(ctrl)
+	m := _mock.NewMockYourInterface(ctrl)
 
 	gomock.InOrder(
-		m.EXPECT().MyInterFunc(gomock.Eq(1)).Return(1).AnyTimes(),
-		m.EXPECT().MyInterFunc(gomock.Any()).Return(-1).AnyTimes(),
+		m.EXPECT().YourInterFunc(gomock.Eq(1)).Return(1).AnyTimes(),
+		m.EXPECT().YourInterFunc(gomock.Eq(2)).Return(2).AnyTimes(),
+		m.EXPECT().YourInterFunc(gomock.Eq(3)).Return(3).AnyTimes(),
 	)
 
 	for _, tc := range tt {
@@ -57,4 +63,3 @@ func TestMyfuncOrder(t *testing.T) {
 		})
 	}
 }
-
