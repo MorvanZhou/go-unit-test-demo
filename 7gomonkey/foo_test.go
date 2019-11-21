901d14d53403cabc785cea4eada95541f9694a44
morvanzhou <morvanzhou@tencent.com>
2019-11-21 13:22:05 +0800

    gomonkey mock

--- 7gomonkey/foo_test.go
+++ 7gomonkey/foo_test.go
@@ -4,33 +4,32 @@ import (
 	"github.com/agiledragon/gomonkey"
 	_gomonkey "github.com/morvanzhou/unittest-demo/7gomonkey"
 	"github.com/stretchr/testify/assert"
-	"log"
 	"testing"
 )
 
 func TestWrapMysqlFunc(t *testing.T) {
 	tt := []struct {
 		name    string
-		input   int
-		mockOut []string
-		expect  string
+		input   int      // 外部输入
+		mockOut []string // mock mysql 的输出
+		expect  []string // 结合外部输入和 mysql 输出的结果
 	}{
-		{"test1", 2, []string{"a2", "b2"}, "2a2"},
-		{"test2", 3, []string{"a1", "b1"}, "3a1"},
+		{"test1", 2, []string{"a2", "b2"}, []string{"2a2", "2b2"}},
+		{"test2", 3, []string{"a1", "b1"}, []string{"3a1", "3b1"}},
+		{"test3", 21, []string{"d10", "f11"}, []string{"21d10", "21f11"}},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 
 			patch := gomonkey.ApplyFunc(_gomonkey.MysqlFunc, func(_ int) []string {
-				log.Printf("%v", tc.input)
 				return tc.mockOut
 			})
 			defer patch.Reset()
 
 			users := _gomonkey.WrapMysqlFunc(tc.input)
 
-			assert.Equal(t, tc.expect, users[0])
+			assert.Equal(t, tc.expect, users)
 
 		})
 	}
